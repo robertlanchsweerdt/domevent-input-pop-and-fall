91dc92986c128251db6a9791f9a10dcb926d4bb3
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-08 16:15:00 -0400

    coded js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const userInput = document.getElementById('user');
+let userOutput = document.getElementById('userOutput');
+const btn = document.querySelector('.btn-submit');
+
+userInput.addEventListener('input', runMe);
+btn.addEventListener('click', clearField);
+
+function runMe() {
+  const message = userInput.value;
+  userOutput.textContent = message;
+
+  console.log(message);
+}
+
+function clearField() {
+  userInput.value = '';
+  userOutput.textContent = '';
+}
