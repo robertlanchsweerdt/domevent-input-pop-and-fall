c2df4d4f9d7365c3f99c012ecb25069f766ccd8a
Robert Lanchsweerdt <robert.lanchsweerdt@gmail.com>
2021-06-08 18:46:50 -0400

    setup heart creation, raining and stopping in js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,33 @@ let userOutput = document.getElementById('userOutput');
 const btn = document.querySelector('.btn-submit');
 
 userInput.addEventListener('input', runMe);
-btn.addEventListener('click', clearField);
+
+btn.addEventListener('click', () => {
+  clearField();
+  rainHearts();
+});
 
 function runMe() {
   const message = userInput.value;
   userOutput.textContent = message;
-
-  console.log(message);
 }
 
 function clearField() {
   userInput.value = '';
   userOutput.textContent = '';
+}
+
+function createHearts() {
+  const heart = document.createElement('div');
+  heart.classList.add('heart');
+  heart.textContent = '❤️';
+
+  document.body.appendChild(heart);
+}
+
+function rainHearts() {
+  const runInterval = setInterval(createHearts, 300);
+  setTimeout(() => {
+    clearInterval(runInterval);
+  }, 5000);
 }
